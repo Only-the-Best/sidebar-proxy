daebea88c37a731779c4cd6ada03de71d0d9752f
jamesj228 <jamesjiang282@gmail.com>
2018-11-07 15:05:37 -0800

    pushing all three modules into proxy file.

--- proxy-server/proxy-server.js
+++ proxy-server/proxy-server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 
 const app = express();
 const port = process.env.port || 3005;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/:houseid', (req, res) => {

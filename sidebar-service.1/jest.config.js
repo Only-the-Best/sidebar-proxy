731306a180098597821ba189a24e230d52e50dd1
jamesj228 <jamesjiang282@gmail.com>
2018-11-11 00:42:06 -0800

    Removed extra files and now have the original files that can be edited.

--- sidebar-service.1/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    globalSetup: './client/src/spec/setup.js',
-    globalTeardown: './client/src/spec/teardown.js',
-    testEnvironment: './client/src/spec/mongo-environment.js',
-    testURL: 'http://localhost/',
-    verbose: true
-  };

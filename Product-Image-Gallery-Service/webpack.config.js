ccc0d9e9e288944fbf641ad16c1bd87c918620ee
jamesj228 <jamesjiang282@gmail.com>
2018-11-11 15:34:08 -0800

    added all the missing package json and webpack config files together

---
+++ Product-Image-Gallery-Service/webpack.config.js
@@ -0,0 +1,33 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
